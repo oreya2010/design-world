ea5bce9a4f4870e6e255515250dd6b3825a9baa4
oreya2010 <oreya2010@yahoo.com>
2016-05-17 19:51:38 -0400

    challenge #1

---
+++ 05172016/app.js
@@ -0,0 +1,26 @@
+var arr = [1,2,3,4,5];
+//#1.1
+var sum = 0;
+for (var i = 0; i < arr.length; i++)
+{
+  sum += arr[i];
+}
+console.log(sum);
+
+/*
+#1.2
+array.splice(2,0,6)
+console.log(array);
+
+before [1,2,3,4,5]
+after[1,2,6,3,4,5]
+
+challengeb#2
+$("main-content span").text("$6500");
+$("main-content h2 span").text("$6500");
+
+$('span')
+$('h2 span')
+$('h2>span')
+
+*/
